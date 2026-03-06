610131110e50a80eb91c5fc8b31c2ee537a214e5
eunicecristina <eunicecristina0510@gmail.com>
2026-03-06 17:48:36 -0300

    commit

--- algoritmos/aula1.py
+++ algoritmos/aula1.py
@@ -1,1 +1,15 @@
-print("teste")
+qrd_prt = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169]
+numero = int(input("Digite um numero: "))
+confirm = False
+
+for i in qrd_prt:
+    for x in qrd_prt:
+        if i + x == numero:
+            confirm = True
+            
+    
+
+if confirm:
+    print("Seu número é um quadrado perfeito resultado de uma soma de dois quadrados perfeitos")
+else:
+    print("Falso")
