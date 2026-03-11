c892f7ccdbb6b30030141bc08ef528b020a985be
eunicecristina <eunicecristina0510@gmail.com>
2026-03-11 13:57:48 -0300

    commit

--- testes/conv_bin/app.py
+++ testes/conv_bin/app.py
@@ -3,6 +3,13 @@ from flask import Flask, render_template, redirect, request, url_for, flash
 app = Flask(__name__)
 app.secret_key = ["projetoinformatica"]
 
-app.route("/")
+app.route("/index", methods=["GET","POST"])
 def index():
-    return render_template("index.html")
+    if request.methods == "POST":
+        num = request.form["numero"]
+        bin = []
+        while num >=1:
+            resto = num % 2
+            bin.insert(0, resto)
+            num = num // 2
+    return render_template("index.html",bin=bin)
