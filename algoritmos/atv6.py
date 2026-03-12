0ef899c7f84508593aaf0df6fcf725d83683cd90
eunicecristina <eunicecristina0510@gmail.com>
2026-03-12 17:46:13 -0300

    commit

---
+++ algoritmos/atv6.py
@@ -0,0 +1,14 @@
+import random
+valores = [20,40,60,80,100]
+hugo = random.choice(valores)
+ze = random.choice(valores)
+luiz = random.choice(valores)
+print(f""""
+####################################
+SORTEIO DA MESADA   
+####################################
+HUGUINHO : R$ {hugo:.2f}   
+ZEZINHO : R$ {ze:.2f}      
+LUIZINHO : R$ {luiz:.2f}   
+####################################
+      """)
