03a5e6a3558d8fbdb47d0d908bb0e4efaa16b14c
eunicecristina <eunicecristina0510@gmail.com>
2026-03-18 17:40:33 -0300

    commit

---
+++ algoritmos/atv8.py
@@ -0,0 +1,11 @@
+###
+### Ops, essa versão contém um erro e não está funcionando!
+###
+print("Cálculo do Salário do Programador")
+horas = float(input("Quantas horas você trabalhou este mês? "))
+valor_hora = float(input("Qual o valor pago por hora? "))
+salario = horas * valor_hora
+print("O seu salário este mês será de R$ %.2f"%salario)
+print("Ah, este mês, o valor da hora tem bônus de 10% 😊😍🤑")
+salario *= 1.1
+print("Com o bônus, o seu salário agora é R$ %.2f "%salario)
--- testes/django/produtos/views.py
+++ testes/django/produtos/views.py
@@ -3,7 +3,14 @@ from django.http import HttpResponse
 
 
 def ver_produto(request):
-    return render(request,'ver_produto.html',{'nome':"Eunice"})
+    
+    if request.method=="GET":
 
+        return render(request,'ver_produto.html',{'nome':"Eunice"})
+    elif request.method=="POST":
+        numero = request.POST.get('numero')
+        valor = request.POST.get('valor')
+        return render(request,'ver_produto.html',{'numero':numero,'nome':"Eunice"})
 def inserir_produto(request):
     return HttpResponse('Inserindo produto')
+        
