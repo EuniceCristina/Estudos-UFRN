c7c2747e73e6a070679f9fc3887f7833e0e4e7ad
eunicecristina <eunicecristina0510@gmail.com>
2026-05-04 16:40:41 -0300

    commit

--- algoritmos/und2/des4.py
+++ algoritmos/und2/des4.py
@@ -8,12 +8,12 @@ fluminense = 0
 palmeiras = 0
 gremio = 0
 santos = 0
-outro = 0
+nenhum = 0
 total = int(input("Quantas pessoas participaram da pesquisa? "))
 
 for i in range(0,total):
     print(f'\nPessoa {i+1}')
-    voto = input('''Qual seu time?\nF - Flamengo, V - Vasco , C- Corinthias, E - Fluminense, P - Palmeiras, G - Grêmio , S - Santos ou O - Outro\n:''').upper()
+    voto = input('''Qual seu time?\nF - Flamengo, V - Vasco , C- Corinthias, E - Fluminense, P - Palmeiras, G - Grêmio , S - Santos ou N - Nenhum\n:''').upper()
     match voto:
         case "F":
             print("Você optou pelo Flamengo!")
@@ -36,9 +36,9 @@ for i in range(0,total):
         case "S":
             print("Você optou pelo Santos!")
             santos+=1
-        case "O":
-            print("Você optou por outro!")
-            outro+=1
+        case "N":
+            print("Você optou por nenhum!")
+            nenhum+=1
         case _:
             print("Opção inválida. Tente novamente")
             total-=1
@@ -49,23 +49,26 @@ percFluminense = fluminense/ total * 100
 percPalmeiras = palmeiras/ total * 100
 percGremio = gremio/ total * 100
 percSantos = santos/ total * 100
-percOutro = outro/ total * 100
+percNenhum = nenhum/ total * 100
+listaPerc = {
+    "1":[percCorinthias,corinthias,"Corinthias"],
+    "2":[percFlamengo,flamengo,"Flamengo"],
+    "3" :[percFluminense,fluminense,"Fluminense"],
+    "4": [percGremio,gremio,"Grêmio"],
+    "5": [percNenhum,nenhum,'Nenhum'],
+    "6":[percPalmeiras,palmeiras,"Palmeiras"],
+    "7":[percSantos,santos,'Santos'],
+    "8":[percVasco,vasco,'Vasco']
+}
+ordenado = sorted(listaPerc.items(), reverse=True, key=lambda x: x[1])
+
+
+    
 print()
 print("#"*40)
+        
 print("\nForam entrevistadas %d pessoas.\n"%total)
-print("Escolheram o Flamengo: %d pessoas\n"%flamengo)
-print(f"Percentual de pessoas que preferem Flamengo: {percFlamengo:.1f}% \n")
-print("Escolheram o Vasco: %d pessoas\n"%vasco)
-print(f"Percentual de pessoas que preferem Vasco: {percVasco:.1f}% \n")
-print("Escolheram o Corinthias: %d pessoas\n"%corinthias)
-print(f"Percentual de pessoas que preferem Corinthias: {percCorinthias:.1f}% \n")
-print("Escolheram o Fluminense: %d pessoas\n"%fluminense)
-print(f"Percentual de pessoas que preferem Fluminense: {percFluminense:.1f}% \n")
-print("Escolheram o Palmeiras: %d pessoas\n"%palmeiras)
-print(f"Percentual de pessoas que preferem Palmeiras: {percPalmeiras:.1f}% \n")
-print("Escolheram o Grêmio: %d pessoas\n"%gremio)
-print(f"Percentual de pessoas que preferem Grêmio: {percGremio:.1f}% \n")
-print("Escolheram o Santos: %d pessoas\n"%santos)
-print(f"Percentual de pessoas que preferem Santos: {percSantos:.1f}% \n")
-print("Escolheram por Outro: %d pessoas\n"%outro)
-print(f"Percentual de pessoas que preferem outro time: {percOutro:.1f}% \n")
+
+for time in ordenado:
+    print(f"Escolheram o {time[1][2]}: %d pessoas\n"%time[1][1])
+    print(f"Percentual de pessoas que preferem {time[1][2]}: {time[1][0]:.1f}% \n")
